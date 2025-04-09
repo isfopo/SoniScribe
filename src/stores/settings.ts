6fc627b37377618a51ecebfcc9c37b259e245199
Isaac Poole <isaacpoolemusic@gmail.com>
2025-04-09 09:11:28 -0400

    ✨ feat(settings): add persistent storage to settings store
    - integrate zustand middleware for state persistence
    - use sessionStorage instead of default localStorage
    - ensure unique name for storage item

--- src/stores/settings.ts
+++ src/stores/settings.ts
@@ -1,12 +1,23 @@
 import { create } from "zustand";
 import { Subdivision } from "../helpers/subdivisions";
+import { persist, createJSONStorage } from "zustand/middleware";
 
 export interface SettingsState {
   subdivision: Subdivision;
   setSubdivision: (subdivision: Subdivision) => void;
 }
 
-export const useSettingsStore = create<SettingsState>((set) => ({
-  subdivision: "whole",
-  setSubdivision: (subdivision) => set({ subdivision }),
-}));
+export const useSettingsStore = create<SettingsState>()(
+  persist(
+    (set) => ({
+      subdivision: "whole",
+      setSubdivision: (subdivision) => {
+        set({ subdivision });
+      },
+    }),
+    {
+      name: "settings-storage", // name of the item in the storage (must be unique)
+      storage: createJSONStorage(() => sessionStorage), // (optional) by default, 'localStorage' is used
+    }
+  )
+);
