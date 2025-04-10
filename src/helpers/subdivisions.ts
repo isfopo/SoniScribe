d3ba58d5e812ff34528c224be43f429ba0e2a385
Isaac Poole <isaacpoolemusic@gmail.com>
2025-04-10 08:20:28 -0400

    ✨ feat(helpers): add value to subdivision points
    - introduce SubdivisionValue type for specific point values
    - extend SubdivisionPointOptions with a value field
    - implement getSubdivisionValue function to retrieve point values

--- src/helpers/subdivisions.ts
+++ src/helpers/subdivisions.ts
@@ -1,27 +1,47 @@
 import { PointOptions } from "peaks.js";
 
-export const SubdivisionPoints: Record<string, Omit<PointOptions, "time">> = {
+export type SubdivisionValue = 1 | 2 | 4 | 8 | 16 | 32 | 64;
+
+export interface SubdivisionPointOptions extends PointOptions {
+  value: SubdivisionValue;
+}
+
+export const SubdivisionPoints: Record<
+  string,
+  Omit<SubdivisionPointOptions, "time">
+> = {
   whole: {
     color: "#fff",
+    value: 1,
   },
   half: {
     color: "#ccc",
+    value: 2,
   },
   quarter: {
     color: "#aaa",
+    value: 4,
   },
   eighth: {
     color: "#999",
+    value: 8,
   },
   sixteenth: {
     color: "#777",
+    value: 16,
   },
   thirtySecond: {
     color: "#666",
+    value: 32,
   },
   sixtyFourth: {
     color: "#555 ",
+    value: 64,
   },
 } as const;
 
 export type Subdivision = keyof typeof SubdivisionPoints;
+
+export const getSubdivisionValue = (
+  subdivision: Subdivision
+): SubdivisionValue => SubdivisionPoints[subdivision].value as SubdivisionValue;
