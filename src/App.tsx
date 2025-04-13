505735d25841607d623baec05a605d8fee8fca20
Isaac Poole <isaacpoolemusic@gmail.com>
2025-04-13 01:01:37 -0400

    ♻️ refactor(App): update useFs hook usage
    - pass an empty object as default parameter to useFs hook for improved consistency and future extensibility.

--- src/App.tsx
+++ src/App.tsx
@@ -20,7 +20,7 @@ function App() {
     }
   };
 
-  const { files, isBrowserSupported, writeFile } = useFs();
+  const { files, isBrowserSupported, writeFile } = useFs({});
 
   const {
     viewRef,
