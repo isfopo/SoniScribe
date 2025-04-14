21e3470f08bd474277da7b7a23e84f4f2c21a3da
Isaac Poole <isaacpoolemusic@gmail.com>
2025-04-14 01:51:06 -0400

    ♻️ refactor(useProjects): replace useState with useRef for current project
    - switch from useState to useRef for currentProject to optimize memory usage
    - update references to currentProject with .current property for consistency

--- src/hooks/useProjects.tsx
+++ src/hooks/useProjects.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useState } from "react";
+import { useCallback, useRef } from "react";
 import { useFileSystem } from "./useFileSystem";
 import { SubdivisionPointOptions } from "../helpers/subdivisions";
 import { stringify } from "../helpers/objects";
@@ -11,20 +11,23 @@ export interface SavedProjectData {
 }
 export const useProjects = () => {
   const { write, entries, remove } = useFileSystem({});
+  const currentProject = useRef<FileSystemFileHandle | undefined>(undefined);
 
-  const [currentProject, setCurrentProject] = useState<
-    FileSystemFileHandle | undefined
-  >();
+  const setCurrentProject = useCallback(
+    (project: FileSystemFileHandle | undefined) => {
+      currentProject.current = project;
+    },
+    [currentProject]
+  );
 
   const addPointsToCurrentProject = useCallback(
     async (points: SubdivisionPointOptions[]) => {
-      if (!currentProject) {
+      if (!currentProject.current) {
         console.error("No current project selected");
         return;
       }
-      const fileHandle = currentProject;
-      const writable = await fileHandle.createWritable();
-      const file = await fileHandle.getFile();
+      const writable = await currentProject.current.createWritable();
+      const file = await currentProject.current.getFile();
       const data = await file.text();
       const projectData = JSON.parse(data) as SavedProjectData;
       projectData.points = [...projectData.points, ...points];
@@ -38,7 +41,7 @@ export const useProjects = () => {
     write,
     remove,
     projects: entries.filter((entry) => entry.name.endsWith(".json")),
-    currentProject,
+    currentProject: currentProject.current,
     setCurrentProject,
     addPointsToCurrentProject,
   };
