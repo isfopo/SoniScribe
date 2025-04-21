c6f5e5b2049690adf943f6118230fbbe980a1a72
Isaac Poole <isaacpoolemusic@gmail.com>
2025-04-21 14:21:13 -0400

    ♻️ refactor(hooks): enhance useEventListener with generic types
    - add generic type parameter T for event type specificity
    - update callback signatures to use the generic event type T
    - improve type safety and flexibility in event listener implementation

--- src/hooks/useEventListener.tsx
+++ src/hooks/useEventListener.tsx
@@ -1,37 +1,36 @@
 import { useRef, useEffect } from "react";
 
-export interface UseEventListenerOptions {
+export interface UseEventListenerOptions<T extends Event> {
   /** The event to listen for. */
   event: string;
   /** The target to listen on. */
   target?: EventTarget | null;
   /** The callback function to call when the event is triggered. */
-  callback: (event: Event) => void;
+  callback: (event: T) => void;
   /** The options for the event listener. */
   options?: AddEventListenerOptions;
   /** The debounce time in milliseconds. */
   debounce?: number;
 }
 
-export const useEventListener = ({
+export const useEventListener = <T extends Event>({
   event,
   target = window,
   callback,
   options,
   debounce = 0,
-}: UseEventListenerOptions) => {
+}: UseEventListenerOptions<T>) => {
   const targetRef = useRef<EventTarget | null>(target || window);
-  const callbackRef = useRef<((event: Event) => void) | null>(callback);
+  const callbackRef = useRef<((event: T) => void) | null>(callback);
   const optionsRef = useRef<AddEventListenerOptions | undefined>(options);
   const debounceTimeoutRef = useRef<number>(new Date().getTime());
 
   useEffect(() => {
     if (targetRef.current && callbackRef.current) {
       const eventTarget = targetRef.current;
-      const _callback = callbackRef.current;
       const _options = optionsRef.current;
 
-      const callbackHandler = (event: Event) => {
+      const callbackHandler = (event: T) => {
         if (
           callbackRef.current &&
           debounceTimeoutRef.current + debounce < new Date().getTime()
@@ -41,10 +40,18 @@ export const useEventListener = ({
         }
       };
 
-      eventTarget.addEventListener(event, callbackHandler, optionsRef.current);
+      eventTarget.addEventListener(
+        event,
+        callbackHandler as EventListenerOrEventListenerObject,
+        optionsRef.current
+      );
 
       return () => {
-        eventTarget.removeEventListener(event, _callback, _options);
+        eventTarget.removeEventListener(
+          event,
+          callbackHandler as EventListenerOrEventListenerObject,
+          _options
+        );
       };
     }
   }, [debounce, event]);
