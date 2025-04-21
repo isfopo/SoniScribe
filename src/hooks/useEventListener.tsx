53acb58132ce0ff04a45792c6048411ec92e1de4
Isaac Poole <isaacpoolemusic@gmail.com>
2025-04-21 08:59:10 -0400

    ✨ feat(useEventListener): add debounce functionality
    - introduce debounce option to delay event callback execution
    - update useEventListener hook to handle debounce timing
    
    🐛 fix(usePeaks): resolve waveform display issue
    
    - add debounce parameter to useEventListener to address bug #7
    - ensure full waveform is shown in overview with new debounce logic

--- src/hooks/useEventListener.tsx
+++ src/hooks/useEventListener.tsx
@@ -9,6 +9,8 @@ export interface UseEventListenerOptions {
   callback: (event: Event) => void;
   /** The options for the event listener. */
   options?: AddEventListenerOptions;
+  /** The debounce time in milliseconds. */
+  debounce?: number;
 }
 
 export const useEventListener = ({
@@ -16,10 +18,12 @@ export const useEventListener = ({
   target = window,
   callback,
   options,
+  debounce = 0,
 }: UseEventListenerOptions) => {
   const targetRef = useRef<EventTarget | null>(target || window);
   const callbackRef = useRef<((event: Event) => void) | null>(callback);
   const optionsRef = useRef<AddEventListenerOptions | undefined>(options);
+  const debounceTimeoutRef = useRef<number>(new Date().getTime());
 
   useEffect(() => {
     if (targetRef.current && callbackRef.current) {
@@ -27,15 +31,21 @@ export const useEventListener = ({
       const _callback = callbackRef.current;
       const _options = optionsRef.current;
 
-      eventTarget.addEventListener(
-        event,
-        callbackRef.current,
-        optionsRef.current
-      );
+      const callbackHandler = (event: Event) => {
+        if (
+          callbackRef.current &&
+          debounceTimeoutRef.current + debounce < new Date().getTime()
+        ) {
+          callbackRef.current(event);
+          debounceTimeoutRef.current = new Date().getTime();
+        }
+      };
+
+      eventTarget.addEventListener(event, callbackHandler, optionsRef.current);
 
       return () => {
         eventTarget.removeEventListener(event, _callback, _options);
       };
     }
-  }, [event]);
+  }, [debounce, event]);
 };
