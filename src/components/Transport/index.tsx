42c87070b37108b795e1658dc583a771c4ea2d89
Isaac Poole <isaacpoolemusic@gmail.com>
2025-08-16 19:07:54 -0400

    Add tooltips and update icons in Transport component

--- src/components/Transport/index.tsx
+++ src/components/Transport/index.tsx
@@ -1,14 +1,17 @@
 import {
-	ArrowBigLeft,
-	ArrowBigLeftDash,
-	ArrowBigRight,
-	ArrowBigRightDash,
-	Pause,
-	Play,
+	BackIcon,
+	PauseIcon,
+	PlayIcon,
+	SkipBackIcon,
+	SkipForwardIcon,
 } from "lucide-react";
-import { ButtonGroup } from "../ButtonGroup";
-import { BarContainer } from "../Container/BarContainer";
-import { Button } from "../ui/button";
+
+import { Button } from "@/components/ui/button";
+import {
+	Tooltip,
+	TooltipContent,
+	TooltipTrigger,
+} from "@/components/ui/tooltip";
 
 export interface TransportProps {
 	playPause: () => void;
@@ -28,28 +31,73 @@ export const Transport = ({
 	end,
 }: TransportProps) => {
 	return (
-		<BarContainer>
-			<ButtonGroup>
-				<Button type="button" onClick={start} title="Start">
-					<ArrowBigLeftDash />
-				</Button>
-
-				<Button type="button" onClick={previousPoint} title="Next Point">
-					<ArrowBigLeft />
-				</Button>
-
-				<Button type="button" onClick={playPause}>
-					{isPlaying ? <Pause /> : <Play />}
-				</Button>
-
-				<Button type="button" onClick={nextPoint} title="Previous Point">
-					<ArrowBigRight />
-				</Button>
-
-				<Button type="button" onClick={end} title="End">
-					<ArrowBigRightDash />
-				</Button>
-			</ButtonGroup>
-		</BarContainer>
+		<div className="divide-primary-foreground/30 inline-flex w-fit divide-x rounded-full shadow-xs">
+			<Tooltip>
+				<TooltipTrigger asChild>
+					<Button
+						onClick={start}
+						className="rounded-none rounded-s-full shadow-none focus-visible:z-10"
+					>
+						<SkipBackIcon />
+						<span className="sr-only">To Start</span>
+					</Button>
+				</TooltipTrigger>
+				<TooltipContent className="px-2 py-1 text-xs">To Start</TooltipContent>
+			</Tooltip>
+			<Tooltip>
+				<TooltipTrigger asChild>
+					<Button
+						onClick={previousPoint}
+						className="rounded-none shadow-none focus-visible:z-10"
+					>
+						<SkipBackIcon />
+						<span className="sr-only">Previous Point</span>
+					</Button>
+				</TooltipTrigger>
+				<TooltipContent className="px-2 py-1 text-xs">
+					Previous Point
+				</TooltipContent>
+			</Tooltip>
+			<Tooltip>
+				<TooltipTrigger asChild>
+					<Button
+						onClick={playPause}
+						className="rounded-none shadow-none focus-visible:z-10"
+					>
+						{isPlaying ? <PauseIcon /> : <PlayIcon />}
+						<span className="sr-only">Play</span>
+					</Button>
+				</TooltipTrigger>
+				<TooltipContent className="px-2 py-1 text-xs">Play</TooltipContent>
+			</Tooltip>
+			<Tooltip>
+				<TooltipTrigger asChild>
+					<Button
+						onClick={nextPoint}
+						className="rounded-none shadow-none focus-visible:z-10"
+					>
+						<SkipForwardIcon />
+						<span className="sr-only">Next Point</span>
+					</Button>
+				</TooltipTrigger>
+				<TooltipContent className="px-2 py-1 text-xs">
+					Next Point
+				</TooltipContent>
+			</Tooltip>
+			<Tooltip>
+				<TooltipTrigger asChild>
+					<Button
+						onClick={end}
+						className="rounded-none rounded-e-full shadow-none focus-visible:z-10"
+					>
+						<SkipForwardIcon />
+						<span className="sr-only">End</span>
+					</Button>
+				</TooltipTrigger>
+				<TooltipContent className="px-2 py-1 text-xs">
+					Skip Forward
+				</TooltipContent>
+			</Tooltip>
+		</div>
 	);
 };
