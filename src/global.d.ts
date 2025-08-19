7d5b61494883ebcf1779f9b1ca0b0b3cfa2f3e63
Isaac Poole <isaacpoolemusic@gmail.com>
2025-08-18 21:33:54 -0400

    Remove unused SVG React component type declaration

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,9 +1,3 @@
 /// <reference types="vite-plugin-svgr/client" />
 
 declare module "*.css";
-
-declare module "*.svg?react" {
-  import React from "react";
-  const ReactComponent: React.FunctionComponent<React.SVGProps<SVGSVGElement>>;
-  export default ReactComponent;
-}
